bdd1e02adbbfa870241da1601d6f1da04169b57f
Richard Harrah <topplethenunnery@gmail.com>
2013-12-18 23:36:41 -0500

    adding @Override annotation and removing empty onDisable()

--- src/main/java/com/bryankeiren/fjord/Fjord.java
+++ src/main/java/com/bryankeiren/fjord/Fjord.java
@@ -5,14 +5,11 @@ import org.bukkit.plugin.java.JavaPlugin;
 
 public final class Fjord extends JavaPlugin {
 	@Override
-	public void onDisable() {
-	}
-
-	@Override
 	public void onEnable() {
 		getCommand("fjord").setExecutor(new FjordCommandExecutor(this));
 	}
 
+	@Override
 	public ChunkGenerator getDefaultWorldGenerator(String worldName, String id) {
 		return new FjordChunkGenerator(this);
 	}
